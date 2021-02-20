739a414e100b8256c23d507f9e7a6dcc71e7caad
Shifaan0010 <53972679+Shifaan0010@users.noreply.github.com>
2021-02-20 18:03:31 +0530

    Using anonymous functions

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-function generate_password(length, lowercase, uppercase, digits, punctuation) {
-    let available_characters = ''
-
-    if (lowercase) {
-        available_characters += 'abcdefghijklmnopqrstuvwxyz'
-    }
-    if (uppercase) {
-        available_characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    }
-    if (punctuation) {
-        available_characters += '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    }
-    if (digits) {
-        available_characters += '0123456789'
-    }
-
-    if (!available_characters || !length) {
-        return 'Password'
+window.addEventListener('load', () => {
+    function generate_password(length, lowercase, uppercase, digits, punctuation) {
+        let available_characters = ''
+    
+        if (lowercase) {
+            available_characters += 'abcdefghijklmnopqrstuvwxyz'
+        }
+        if (uppercase) {
+            available_characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        }
+        if (punctuation) {
+            available_characters += '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+        }
+        if (digits) {
+            available_characters += '0123456789'
+        }
+    
+        if (!available_characters || !length) {
+            return 'Password'
+        }
+        
+        let password = ''
+    
+        for (let i = 0; i < length; i += 1) {
+            password += available_characters.charAt(Math.floor(Math.random() * available_characters.length))
+        }
+    
+        return password
     }
     
-    let password = ''
-
-    for (let i = 0; i < length; i += 1) {
-        password += available_characters.charAt(Math.floor(Math.random() * available_characters.length))
-    }
-
-    return password
-}
-
-function main() {
     const generate_button = document.querySelector('#password_generator #generate_button')
 
     const password_h1 = document.querySelector('#password_generator #password_h1')
@@ -39,6 +39,4 @@ function main() {
     const punctuation_checkbox = document.querySelector('#password_generator #punctuation_checkbox')
 
     generate_button.addEventListener('click', () => password_h1.innerText = generate_password(Number(length_slider.value), lowercase_checkbox.checked, uppercase_checkbox.checked, digits_checkbox.checked, punctuation_checkbox.checked))
-}
-
-window.addEventListener('load', main)
+})
